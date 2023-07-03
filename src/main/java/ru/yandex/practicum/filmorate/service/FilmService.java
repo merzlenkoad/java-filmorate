62d65fe94f7f2a214259b49cb7c4e864bb2a8492
Merzlenko <merzlenkoad@gmail.com>
2023-07-03 09:43:53 +0300

    commit Sprint 10 checkStyle4

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -7,17 +7,10 @@ import java.util.List;
 public interface FilmService {
 
     Film addFilm(Film film);
-
     Film updateFilm(Film film);
-
     List<Film> getAll();
-
     Film getFilm(Integer id);
-
     void addLike(Integer filmId, Integer userId);
-
     void deleteLike(Integer filmId, Integer userId);
-
     List<Film> getPopularFilms(String count);
-
 }
